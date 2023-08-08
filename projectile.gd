d1669322d230ba8dc9a63d3ad4c6f3e29aea20f9
Jackson Arnold <jackson.n.arnold@gmail.com>
2023-08-07 22:28:53 -0400

    Projectile automatically deletes itself
    08/07/23

--- projectile.gd
+++ projectile.gd
@@ -3,14 +3,21 @@ extends Area2D
 var speed = 40
 var direction = 1 # 1 for Up and -1 for down
 
+var screen_size
+var projectile_length
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	# Set these for deleting the projectile automatically if it fully goes off screen
+	screen_size = get_viewport_rect().size
+	projectile_length = $CollisionShape2D.shape.size.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += Vector2(0, direction * -speed) * delta
+	
+	if(position.y <= 0 - projectile_length or position.y >= screen_size.y + projectile_length):
+		queue_free()
 
 
 func _on_area_entered(area):
